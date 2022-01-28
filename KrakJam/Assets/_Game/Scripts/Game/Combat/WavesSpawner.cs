0140e2a202de7d49e1c745bdf2fd54b35f61f5a1
Oskar Kogut <kogutoskar123@gmail.com>
2022-01-28 04:55:42 +0100

    Add waves spawning

--- KrakJam/Assets/_Game/Scripts/Game/Combat/WavesSpawner.cs
+++ KrakJam/Assets/_Game/Scripts/Game/Combat/WavesSpawner.cs
@@ -7,12 +7,71 @@ namespace Game.Combat
 {
     public class WavesSpawner : MonoBehaviour
     {
+        [SerializeField] private List<Wave> waves;
+        [SerializeField] [Range(0,100)] private float xSpawnSpace = 10;
+        [SerializeField] [Range(0,100)] private float ySpawnSpace = 10;
+
+        private int enemiesLeft;
+        private int currentWave = 0;
+        private AI.Astar.Grid grid;
         
+        private void Start()
+        {
+            grid = FindObjectOfType<AI.Astar.Grid>();
+            Spawn(waves[0]);
+        }
+
+        private void Spawn(Wave wave)
+        {
+            enemiesLeft = wave.Enemies.Count;
+
+            foreach (var enemy in wave.Enemies)
+            {
+                Vector2 pos;
+                var spawnedEnemy = Instantiate(enemy, SpawnPos(), Quaternion.identity);
+                spawnedEnemy.Death += UpdateCounter;
+            }
+        }
+
+        private void UpdateCounter()
+        {
+            enemiesLeft--;
+
+            if (enemiesLeft <= 0)
+            {
+                currentWave++;
+                if (waves.Count > currentWave)
+                {
+                    Spawn(waves[currentWave]);
+                }
+                else
+                {
+                    print("gg");
+                }
+            }
+        }
+
+        private Vector2 SpawnPos()
+        {
+            Vector2 targetPos;
+            
+            while (true)
+            {
+                targetPos = new Vector2(UnityEngine.Random.Range(-xSpawnSpace / 2, xSpawnSpace / 2), UnityEngine.Random.Range(-ySpawnSpace / 2, ySpawnSpace / 2));
+                
+                if(grid.IsPositionWalkable(targetPos))
+                    break;
+            }
+
+            return targetPos;
+        }
     }
 
     [Serializable]
     public class Wave
     {
-        
+        [SerializeField] private List<EnemyHealth> enemies;
+
+        public List<EnemyHealth> Enemies => enemies;
     }
 }
